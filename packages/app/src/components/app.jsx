42b7ee369b1fd8c68f86a4898aef9d37281139d4
Lukas Bombach <lukas.bombach@googlemail.com>
2019-04-15 16:55:20 +0200

    minimal setup

---
+++ packages/app/src/components/app.jsx
@@ -0,0 +1,3 @@
+import { h } from "preact";
+
+export default () => <div>Hello World!</div>;
---
+++ packages/app/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src"),
+  output: {
+    path: path.resolve(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
