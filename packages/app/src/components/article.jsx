f8e1a16d54b240f95df3442d1567462037890a57
Lukas Bombach <lukas.bombach@googlemail.com>
2019-04-15 17:28:39 +0200

    partial hydration without render root

--- packages/app/src/components/article.jsx
+++ packages/app/src/components/article.jsx
@@ -1,15 +1,29 @@
-import { h, Component } from "preact";
+import { h } from "preact";
+// import { Component } from "preact";
+import { Component } from "preact/src/component";
 
 export default class Article extends Component {
+  state = {
+    counter: 1
+  };
+
   componentDidMount() {
     console.log("Article did mount");
   }
 
+  increase() {
+    this.setState(state => {
+      const counter = state.counter + 1;
+      console.log("Inc to", counter);
+      return { counter };
+    });
+  }
+
   render() {
     return (
-      <article onClick={() => console.log("clicked on Article")}>
-        <h2>Fermentum Amet Aenean</h2>
-        <p onClick={() => console.log("clicked on P")}>
+      <article onClick={() => this.increase()}>
+        <h2>Fermentum Amet Aenean ({this.state.counter})</h2>
+        <p>
           Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit.
         </p>
